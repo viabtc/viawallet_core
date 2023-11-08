60086957cab7fa031667359938b65077171a089f
Zeev Zhao <zeev.zhao@vinotech.com>
2023-11-08 09:08:12 +0800

    build: aar打包流程优化；

---
+++ tools/apply_environment.py
@@ -0,0 +1,70 @@
+import sys
+import json
+
+args = sys.argv
+environment = args[1]
+
+with open('registry.json') as file:
+    data = json.load(file)
+
+if environment == 'default_mainnet':
+    for item in data:
+        if item['id'] == 'classic':
+            if 'slip44' in item:
+                del item['slip44']
+            for derivation in item['derivation']:
+                derivation['path'] = "m/44'/61'/0'/0/0"
+        if item['id'] == 'bitcoin':
+            item['p2pkhPrefix'] = 0
+            item['p2shPrefix'] = 5
+        if item['id'] == 'litecoin':
+            item['p2pkhPrefix'] = 48
+            item['p2shPrefix'] = 50
+        if item['id'] == 'doge':
+            item['p2pkhPrefix'] = 30
+            item['p2shPrefix'] = 22
+        if item['id'] == 'bitcoincash':
+            item['p2pkhPrefix'] = 0
+            item['p2shPrefix'] = 5
+            item['hrp'] = 'bitcoincash'
+elif environment == 'coldwallet_mainnet':
+    for item in data:
+        if item['id'] == 'classic':
+            item['slip44'] = 60
+            for derivation in item['derivation']:
+                derivation['path'] = "m/44'/60'/0'/0/0"
+        if item['id'] == 'bitcoin':
+            item['p2pkhPrefix'] = 0
+            item['p2shPrefix'] = 5
+        if item['id'] == 'litecoin':
+            item['p2pkhPrefix'] = 48
+            item['p2shPrefix'] = 50
+        if item['id'] == 'doge':
+            item['p2pkhPrefix'] = 30
+            item['p2shPrefix'] = 22
+        if item['id'] == 'bitcoincash':
+            item['p2pkhPrefix'] = 0
+            item['p2shPrefix'] = 5
+            item['hrp'] = 'bitcoincash'
+elif environment == 'coldwallet_testnet':
+    for item in data:
+        if item['id'] == 'classic':
+            item['slip44'] = 60
+            for derivation in item['derivation']:
+                derivation['path'] = "m/44'/60'/0'/0/0"
+        if item['id'] == 'bitcoin':
+            item['p2pkhPrefix'] = 111
+            item['p2shPrefix'] = 196
+        if item['id'] == 'litecoin':
+            item['p2pkhPrefix'] = 111
+            item['p2shPrefix'] = 58
+        if item['id'] == 'doge':
+            item['p2pkhPrefix'] = 113
+            item['p2shPrefix'] = 196
+        if item['id'] == 'bitcoincash':
+            item['p2pkhPrefix'] = 111
+            item['p2shPrefix'] = 196
+            item['hrp'] = 'bchtest'
+
+with open('registry.json', 'w') as outfile:
+    json.dump(data, outfile, indent=2)
